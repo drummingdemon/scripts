de304ddd68303fb8044914092440710097c76ab6
Gábor Hargitai <drummingdemon@users.noreply.github.com>
2019-06-14 23:51:22 +1200

    added install_ffmpeg.sh
    accompanies the post over at my blog

---
+++ install_ffmpeg.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+brew tap varenc/ffmpeg
+brew tap-pin varenc/ffmpeg
+brew install ffmpeg --with-chromaprint --with-fdk-aac --with-librsvg  --with-libsoxr --with-libssh --with-tesseract --with-libvidsta --with-opencore --with-openh264 --with-openjpeg --with-openssl  --with-rubberban --with-webp --with-zeromq --with-zimg --with-srt --with-libvmaf  --with-libxml2 --with-g --with-libbluray --with-libbs2b --with-libcaca --with-libgsm --with-libmodplu --with-rtmpdump --with-speex --with-two-lame --with-wavpack --with-xvid
